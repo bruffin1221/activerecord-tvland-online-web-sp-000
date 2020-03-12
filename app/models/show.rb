7d8bc32e159b9f8346ecec06e2e36e705c65f160
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-12 16:48:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :character
+  has_many :actors, through: :characters
+
+def actors_list
+  actors_id.counts
+end
+
 end
