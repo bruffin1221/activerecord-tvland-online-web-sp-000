b054a0a2ab00176c1ff8e25a1a5f10ef34d1184b
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-12 18:10:19 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_catchphrase_to_characters.rb
+++ db/migrate/008_add_catchphrase_to_characters.rb
@@ -1,6 +1,6 @@
 class AddCatchphrasetoCharacters < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :character, :catchphrase, :string
   end
 end
